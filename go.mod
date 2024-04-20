804512b01e057ccb79ffd1c9d1dc2d0dc082c332
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-04-20 16:01:00 +0000

    Write start bot message
    Write start code of my project

---
+++ go.mod
@@ -0,0 +1,22 @@
+module myprojeckt
+
+go 1.22.2
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/bytedance/sonic v1.11.3 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.1 // indirect
+	github.com/fasthttp/router v1.5.0 // indirect
+	github.com/grbit/go-json v0.11.0 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
+	github.com/mymmrac/telego v0.29.2 // indirect
+	github.com/savsgio/gotils v0.0.0-20240303185622-093b76447511 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.52.0 // indirect
+	github.com/valyala/fastjson v1.6.4 // indirect
+	golang.org/x/arch v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mymmrac/telego"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
+)
+
+// Структура для розкодування JSON-відповіді
+type WeatherData struct {
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func main() {
+	botToken := "6498753115:AAEEl4xSkVGvuiXLgdbWGk-KNeEcecT8tLc"
+
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	updates, _ := bot.UpdatesViaLongPolling(nil)
+
+	bh, _ := th.NewBotHandler(bot, updates)
+
+	defer bh.Stop()
+	defer bot.StopLongPolling()
+
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		chatID := tu.ID(update.Message.Chat.ID)
+
+		message := tu.Message(
+			chatID,
+			"Привіт! В цьому боті ви можете дізнаватись температуру повітр'я в різних містах України! Щоб почати введіть /та назву свого міста. Успіхів!",
+		)
+
+		bot.SendMessage(message)
+
+	}, th.CommandEqual("start"))
+
+	bh.Start()
+
+}
+
+func weather() {
+	apiKey := "3f7c7314bbddea4af2f8175638c88ad6"
+	// URL для запиту погоди в Києві
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=Ternopil&units=metric&appid=%s", apiKey)
+
+	// Виконати GET-запит
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Помилка під час виконання запиту: %s", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Прочитати відповідь у вигляді масиву байтів
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Помилка при читанні відповіді: %s", err)
+		return
+	}
+
+	// Розкодувати JSON-відповідь
+	var data WeatherData
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Printf("Помилка при розкодуванні JSON: %s", err)
+		return
+	}
+
+	// Вивести температуру
+	fmt.Printf("Погода в Києві: %.1f°C\n", data.Main.Temp)
+}
