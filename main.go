952ee15fb1792b34e9304aa1b9f32e1205e93d99
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-05-25 11:32:45 +0000

    Update main.go

--- main.go
+++ main.go
@@ -249,7 +249,7 @@ func ALLINone(town string, chatid telego.ChatID, bot *telego.Bot, URL string) {
 	TEMP := weatherResponse.Data[0].Temp
 	description := weatherResponse.Data[0].Weather.Description
 
-	tempcity := fmt.Sprintf("Температура повітря в %.1f°C\\n", TEMP, "Опис погоди:%s\\n", description)
+	tempcity := fmt.Sprintf("Температура повітря в %.1f°C\n", TEMP, "Опис погоди:%s\n", description)
 
 	message1 := tu.Message(
 		chatid,
